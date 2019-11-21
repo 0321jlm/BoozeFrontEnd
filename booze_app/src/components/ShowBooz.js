8e0d0e7131203105ff2f928f326cf30007693808
Fred Kaesmann <fredkaesmann@UDSAdmins-MacBook-Pro.local>
2019-11-21 16:26:43 -0500

    D

--- booze_app/src/components/ShowBooz.js
+++ booze_app/src/components/ShowBooz.js
@@ -5,15 +5,15 @@ class ShowBooz extends React.Component {
     return (
       <div>
         <h3>Brewery Info</h3>
-        <h4>{this.props.booz.details.name}</h4>
-        <h6>Type: {this.props.booz.details.brewery_type}</h6>
-        <h6>Street: {this.props.booz.details.street}</h6>
-        <h6>City: {this.props.booz.details.city}</h6>
-        <h6>State: {this.props.booz.details.state}</h6>
-        <h6>Zip: {this.props.booz.details.postal_code}</h6>
-        <h6>Phone {this.props.booz.details.phone}</h6>
-        <h6>Website: {this.props.booz.details.website_url}</h6>
-        <p>Comments: {this.props.booz.comments}</p>
+        <h4>{this.props.booz.name}</h4>
+        <h6>Type: {this.props.booz.brewery_type}</h6>
+        <h6>Street: {this.props.booz.street}</h6>
+        <h6>City: {this.props.booz.city}</h6>
+        <h6>State: {this.props.booz.state}</h6>
+        <h6>Zip: {this.props.booz.postal_code}</h6>
+        <h6>Phone {this.props.booz.phone}</h6>
+        <h6>Website: {this.props.booz.website_url}</h6>
+        <p>Comments: {this.props.booz2.comments}</p>
       </div>
     );
   }
