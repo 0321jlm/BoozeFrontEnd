d9684d05f01c418a641303be0e248b7366b396c0
Joann Briggs <joannbriggs@Joanns-MacBook-Pro.local>
2019-11-21 16:21:00 -0500

    joanns day 1 commit

--- booze_app/src/App.js
+++ booze_app/src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import "../src/index.css";
+
+// import Booz from "./components/Booz.js";
+import NewBooz from "./components/NewBooz.js";
+
+let baseURL = process.env.REACT_APP_BASEURL;
+
+if (process.env.NODE_ENV === "development") {
+  baseURL = "http://localhost:3001";
+} else {
+  baseURL = "https://fathomless-sierra-68956.herokuapp.com";
+}
+
+// console.log("current base URL:", baseURL);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: "",
+      name: "",
+      url: "",
+      booz: [],
+      addBooz: ""
+    };
+    this.handleNewBooz = this.handleNewBooz.bind(this);
+  }
+
+  handleNewBooz(aBooz) {
+    console.log("handle new booz entered: ", aBooz);
+    // console.log("this.sate.booz: ", this.state.booz);
+    // this.setState({
+    //   booz: [...this.state.booz, aBooz]
+    // });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>BOOZ HEADER</h1>
+        {/* <Booz /> */}
+        <NewBooz handleNewBooz={this.handleNewBooz} />
+      </div>
+    );
+  }
 }
 
 export default App;
