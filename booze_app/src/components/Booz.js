21076d25974bee8e49033d4a468d574d7675b1f5
0321jlm <jessica.madeux@prudential.com>
2019-11-22 10:42:41 -0500

    changed frontend port

--- booze_app/src/components/Booz.js
+++ booze_app/src/components/Booz.js
@@ -5,7 +5,7 @@ import UpdateBooz from "./UpdateBooz";
 import axios from "axios";
 let baseURL = process.env.REACT_APP_BASEURL;
 if (process.env.NODE_ENV === "development") {
-  baseURL = "http://localhost:3000";
+  baseURL = "http://localhost:3003";
 }
 
 class Booz extends Component {
